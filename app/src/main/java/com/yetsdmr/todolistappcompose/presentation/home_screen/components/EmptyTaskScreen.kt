5a582bc3cc72dba4509f2e1dddc1453cb2748bff
Yunus Emre Taşdemir <emre.tasdemir@btc-ag.com.tr>
2024-01-23 15:52:42 +0300

    Added empty task screen.

---
+++ app/src/main/java/com/yetsdmr/todolistappcompose/presentation/home_screen/components/EmptyTaskScreen.kt
@@ -0,0 +1,30 @@
+package com.yetsdmr.todolistappcompose.presentation.home_screen.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EmptyTaskScreen(paddingValues: PaddingValues) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "No Tasks!",
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 28.sp
+        )
+    }
+}
